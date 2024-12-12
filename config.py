3fd43390dd159740007f2dc531ecfa01dd4fc346
Parthiv Katpara <83153745+satyanandatripathi@users.noreply.github.com>
2024-12-12 15:19:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,28 @@
 import os
-
+from dotenv import load_dotenv
 
 class Config:
+    # Check if the .env file exists and load it
+    try:
+        if os.path.exists(".env"):
+            load_dotenv()
+        else:
+            raise FileNotFoundError(".env file is missing.")
+    except FileNotFoundError as e:
+        print(e)
+
     # Pyrogram Configuration
-    API_ID = int(os.getenv("API_ID", "123456"))  # Replace with your API ID
-    API_HASH = os.getenv("API_HASH", "your_api_hash")  # Replace with your API Hash
-    SESSION_STRING = os.getenv("SESSION_STRING", "your_session_string")  # Replace with your Session String
+    API_ID = int(os.getenv("API_ID", "123456"))  # Default to 123456 if not set
+    API_HASH = os.getenv("API_HASH", "your_api_hash")
+    SESSION_STRING = os.getenv("SESSION_STRING", "your_session_string")
 
     # MongoDB Configuration
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")  # Replace with your MongoDB URI
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 
     # Redis Configuration
-    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")  # Replace with your Redis URL
+    REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+    REDIS_PASS = os.getenv("REDIS_PASS", "NON")
 
     # Throwbin API Key
-    #THROWBIN_API_KEY = os.getenv("THROWBIN_API_KEY", "your_throwbin_api_key")  # Replace with your Throwbin API Key
+    # THROWBIN_API_KEY = os.getenv("THROWBIN_API_KEY", "your_throwbin_api_key")
+    
